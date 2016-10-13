13588b4fe0bc6f24b56c6916ccd3855ca6540167
Tuomas Liikala <tuomas.liikala@student.tut.fi>
2016-10-13 22:39:46 +0300

    tests
    -

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,20 @@
 # content of test_sample.py
+
+from flask import Flask, render_template, json, request,redirect,session,jsonify
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'fff!!FFDddddsaa/67'
+app.config['MYSQL_DATABASE_DB'] = 'qvantel'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 def func(x):
     return x + 1
 
